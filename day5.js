103ed4eec58600632296fb2de21ea68d8a5d9212
Ashwin L M <ashwincricket004@gmail.com>
2026-06-14 18:05:09 +0530

    day5 added

---
+++ day5.js
@@ -0,0 +1,32 @@
+// function findSmallest(arr){
+//     let smallest = arr[0]
+
+//     for(let i = 0; i < arr.length ; i++){
+//         if(arr[i] < smallest){
+//             smallest = arr[i]
+//         }
+//     }
+//     return smallest
+
+// }
+// console.log(findSmallest([22,54,33,11,45]));
+
+// function largestNumber(arr){
+//     let largest = arr[0]
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > largest){
+//             largest = arr[i]
+//         }
+//     }
+//     return largest
+// }
+
+let n = 4
+
+function square(num){
+    let ans = num * num
+    // return ans
+}
+
+console.log(square(n));
