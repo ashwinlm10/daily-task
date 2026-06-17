05181f5c5498bc10eee4b407dc61b7f0c1fa56f1
Ashwin L M <ashwincricket004@gmail.com>
2026-06-17 22:50:44 +0530

    scope file has been added

---
+++ scope.js
@@ -0,0 +1,80 @@
+// let a = 10
+
+// function outer(){
+//     let b = 20
+
+//     function inner(){
+//         let c = 210
+//         console.log(c);
+//         console.log(b);
+//         console.log(a);
+        
+//     }
+//     inner()
+// }
+// outer()
+
+
+// function a(){
+//     var b = 10
+
+//     c()
+//     function c(){
+//         console.log(b);
+        
+//     }
+// }
+// a()
+
+// console.log(z);
+
+// let z = 10
+
+
+// console.log(x);
+
+// const x = 20
+//in tdz let and const variable are hoisted when i00
+
+//find the largest number
+
+function findLargest(arr){
+    let largest = arr[0]
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > largest){
+            largest = arr[i]
+        }
+    }
+    return largest
+}
+console.log(findLargest([3, 7, 2, 9, 4]));
+
+
+//Write a function countVowels(str) that counts
+
+function countVowels(str){
+    let count = 0
+    let vowels = "aeiou"
+    
+    for(let i = 0; i < str.length; i++){
+        if(vowels.includes(str[i])){
+            count++
+        }
+    }
+    return count
+}
+console.log(countVowels('hello'));
+console.log(countVowels('nin akkan'));
+
+
+// Reverse a string
+
+function reverseString(str){
+    let reversed = ''
+    for(let i = str.length-1; i>=0; i--){
+        reversed+= str[i]
+    }
+    return reversed
+}
+console.log(reverseString('nin amman'));
+console.log(reverseString('ad est marithya meri'));
