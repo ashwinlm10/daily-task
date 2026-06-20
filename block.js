273864144ce6720da470bbe60d9ee3ed1fa7c9fe
Ashwin L M <ashwincricket004@gmail.com>
2026-06-20 19:37:06 +0530

    new files added

---
+++ block.js
@@ -0,0 +1,10 @@
+{
+    let a = 10
+    var b = 20
+    const c = 30
+
+    
+}
+console.log(b);
+console.log(a);
+console.log(c);
---
+++ day6.js
@@ -0,0 +1,41 @@
+function duplicate(arr){
+    let result = []
+
+    for(let i = 0; i <arr.length; i++){
+        if(!result.includes(arr[i])){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+console.log(duplicate([1,1,2,3,10,16,10,16]));
+
+
+function removeDuplicate(str){
+    let result = ''
+
+    for(let i = 0; i < str.length; i++){
+        if(!result.includes(str[i])){
+            result += str[i]
+        }
+    }
+    return result
+}
+console.log(removeDuplicate(['k', 'a', 'a', 'v', 'y', 'a']));
+
+function remDup(arr){
+    return [... new Set(arr)]
+}
+console.log(remDup([1,2,3,4,5,6,6]));
+
+
+function sumofDigit(n){
+    let sum = 0
+    let digit = n.toString().split('')
+
+    for(let i = 0; i < digit.length; i++){
+        sum += Number(digit[i])
+    }
+    return sum
+}
+console.log(sumofDigit(123));
