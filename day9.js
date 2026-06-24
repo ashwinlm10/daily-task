488e55d7814dde1ba81d55eb63b9b64a59256aea
Ashwin L M <ashwincricket004@gmail.com>
2026-06-24 23:21:10 +0530

    day9 uploaded

--- day9.js
+++ day9.js
@@ -15,4 +15,30 @@ console.log(countChar('banana', 'a'))    // 3
 console.log(countChar('javascript', 's')) // 2
 console.log(countChar('hello', 'z')) 
 
-//
+//closure + setTimeout
+
+// function x(){
+//     for(let i = 0; i <= 5; i++){
+//         setTimeout(function(){
+//             console.log(i);
+//         }, 1000);
+//     }
+//     console.log('namaste sir');
+// }
+// x()
+
+//closure + setTimeout with var not let to print 1,2,3,4,5,6
+
+
+function x(){
+    for(var i=1;i<=5;i++){
+        function close(i){
+            setTimeout(function(){
+                console.log(i);
+            }, i*1000)
+        }
+        close(i)
+    }
+    console.log('namaste'); 
+}
+x()
