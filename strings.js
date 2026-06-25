acac07dee324bd576cdbfc5664a1ddee7d5ab260
Ashwin L M <ashwincricket004@gmail.com>
2026-06-25 20:23:45 +0530

    strings.js added

---
+++ strings.js
@@ -0,0 +1,45 @@
+//1. Convert "hello world" to uppercase
+
+let string = "hello world"
+console.log(string.toUpperCase());
+
+//Find the length of "JavaScript"
+let str = "javascript"
+console.log(str.length);
+
+//Check if "banana" contains "nan"
+let string1 = "banana"
+console.log(string1.includes("nan"));
+
+//Replace "cat" with "dog" in "I have a cat"
+let sentence = "I have a cat"
+console.log(sentence.replace("cat", "dog"));
+
+//Split "red,green,blue" into an array
+let color = "red,greeen, blue"
+console.log(color.split(","));
+
+//Reverse the string "hello"
+let string2 = "hello"
+console.log(string2.split('').reverse("").join(""));
+
+//Capitalize the first letter of "ashwin"
+let name = "javascript"
+let result = name.slice(0,3)+
+name.charAt(3).toUpperCase()+
+name.slice(4)
+console.log(result);
+
+
+
+//Count how many times "a" appears in "banana"
+let string4 = 'banana'
+let count = 0
+for(let i=0;i<=string4.length;i++){
+    if(string4[i] === 'a'){
+        count++
+    }
+}
+console.log(count);
+
+
